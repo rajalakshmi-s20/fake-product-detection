6d696b106cdbd88674b2783a1f1c3e75ff142e39
RAJALAKSHMI.S <rajalakshmi.sarangan20@gmail.com>
2023-02-26 22:05:28 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Login/>
     </div>
   );
 }

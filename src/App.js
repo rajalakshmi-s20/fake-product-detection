82e26512676f6c65f1b513921c43a6998b992615
RAJALAKSHMI.S <rajalakshmi.sarangan20@gmail.com>
2023-03-31 23:59:55 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Homepage from './components/Homepage';
 import QrContainer from './components/QrContainer';
@@ -11,14 +11,14 @@ import './App.css';
 function App() {
 
   return (
-    <div>
       <BrowserRouter>
-        <Route exact path="/" component={Homepage}/>
-        <Route exact path="/login" component={Login}/>
-        <Route exact path="/qrgenerator" component={QrGenerator}/>
-        <Route exact path="/qrcontainor" component={QrContainer}/>
+        <Routes>
+          <Route exact path="/" element={<Homepage/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/qrgenerator" element={<QrGenerator/>}/>
+          <Route exact path="/qrcontainor" element={<QrContainer/>}/>
+        </Routes>
       </BrowserRouter>
-    </div>
   );
 }
 

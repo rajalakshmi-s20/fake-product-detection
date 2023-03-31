7469f795779a211b98148c95010f4f31dc2fe2fb
RAJALAKSHMI.S <rajalakshmi.sarangan20@gmail.com>
2023-04-01 00:14:29 +0530

    route

--- src/components/Login-components.js
+++ src/components/Login-components.js
@@ -1,69 +1,67 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-background-color: #fff;
-border-radius: 10px;
-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-position: relative;
-overflow: hidden;
-width: 678px;
-max-width: 100%;
-min-height: 400px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  position: relative;
+  overflow: hidden;
+  width: 678px;
+  max-width: 100%;
+  min-height: 400px;
 `;
 
 export const SignUpContainer = styled.div`
- position: absolute;
- top: 0;
- height: 100%;
- transition: all 0.6s ease-in-out;
- left: 0;
- width: 50%;
- opacity: 0;
- z-index: 1;
- ${props => props.signinIn !== true ? `
-   transform: translateX(100%);
-   opacity: 1;
-   z-index: 5;
- ` 
- : null}
+  position: absolute;
+  top: 0;
+  height: 100%;
+  transition: all 0.6s ease-in-out;
+  left: 0;
+  width: 50%;
+  opacity: 0;
+  z-index: 1;
+  ${props => props.signinIn !== true ? `
+    transform: translateX(100%);
+    opacity: 1;
+    z-index: 5;
+  ` 
+  : null}
 `;
 
-
 export const SignInContainer = styled.div`
-position: absolute;
-top: 0;
-height: 100%;
-transition: all 0.6s ease-in-out;
-left: 0;
-width: 50%;
-z-index: 2;
-${props => (props.signinIn !== true ? `transform: translateX(100%);` : null)}
+  position: absolute;
+  top: 0;
+  height: 100%;
+  transition: all 0.6s ease-in-out;
+  left: 0;
+  width: 50%;
+  z-index: 2;
+  ${props => (props.signinIn !== true ? `transform: translateX(100%);` : null)}
 `;
 
 export const Form = styled.form`
-background-color: #ffffff;
-display: flex;
-align-items: center;
-justify-content: center;
-flex-direction: column;
-padding: 0 50px;
-height: 100%;
-text-align: center;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 50px;
+  height: 100%;
+  text-align: center;
 `;
 
 export const Title = styled.h1`
-font-weight: bold;
-margin: 0;
+  font-weight: bold;
+  margin: 0;
 `;
 
 export const Input = styled.input`
-background-color: #eee;
-border: none;
-padding: 12px 15px;
-margin: 8px 0;
-width: 100%;
+  background-color: #eee;
+  border: none;
+  padding: 12px 15px;
+  margin: 8px 0;
+  width: 100%;
 `;
-
 
 export const Button = styled.button`
   border-radius: 20px;
@@ -83,28 +81,30 @@ export const Button = styled.button`
       outline: none;
   }
 `;
+
 export const GhostButton = styled(Button)`
-background-color: transparent;
-border-color: #ffffff;
+  background-color: transparent;
+  border-color: #ffffff;
 `;
 
 export const Anchor = styled.a`
-color: #333;
-font-size: 14px;
-text-decoration: none;
-margin: 15px 0;
+  color: #333;
+  font-size: 14px;
+  text-decoration: none;
+  margin: 15px 0;
 `;
+
 export const OverlayContainer = styled.div`
-position: absolute;
-top: 0;
-left: 50%;
-width: 50%;
-height: 100%;
-overflow: hidden;
-transition: transform 0.6s ease-in-out;
-z-index: 100;
-${props =>
- props.signinIn !== true ? `transform: translateX(-100%);` : null}
+  position: absolute;
+  top: 0;
+  left: 50%;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+  transition: transform 0.6s ease-in-out;
+  z-index: 100;
+  ${props =>
+  props.signinIn !== true ? `transform: translateX(-100%);` : null}
 `;
 
 export const Overlay = styled.div`
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,55 +2,54 @@ import React from "react";
 import * as Components from './Login-components.js';
 
 export default function Login() {
-      const [signIn, toggle] = React.useState(true);
-       return(
-           <Components.Container>
-               <Components.SignUpContainer signinIn={signIn}>
-                   <Components.Form>
-                       <Components.Title>Create Account</Components.Title>
-                       <Components.Input type='text' placeholder='Name' id='name'/>
-                       <Components.Input type='email' placeholder='Email' id='email'/>
-                       <Components.Input type='password' placeholder='Password' id='pass'/>
-                       <Components.Button>Sign Up</Components.Button>
-                   </Components.Form>
-               </Components.SignUpContainer>
- 
-               <Components.SignInContainer signinIn={signIn}>
-                    <Components.Form>
-                        <Components.Title>Sign in</Components.Title>
-                        <Components.Input type='email' placeholder='Email' id='email'/>
-                        <Components.Input type='password' placeholder='Password' id='pass'/>
-                        <Components.Button>Sign In</Components.Button>
-                        <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
-                    </Components.Form>
-               </Components.SignInContainer>
- 
-               <Components.OverlayContainer signinIn={signIn}>
-                   <Components.Overlay signinIn={signIn}>
- 
-                   <Components.LeftOverlayPanel signinIn={signIn}>
-                       <Components.Title>Welcome Back!</Components.Title>
-                       <Components.Paragraph>
-                           To keep connected with us please login with your personal info
-                       </Components.Paragraph>
-                       <Components.GhostButton onClick={() => toggle(true)}>
-                           Sign In
-                       </Components.GhostButton>
-                       </Components.LeftOverlayPanel>
- 
-                       <Components.RightOverlayPanel signinIn={signIn}>
-                         <Components.Title>Hello, Friend!</Components.Title>
-                         <Components.Paragraph>
-                             Enter Your personal details and start journey with us
-                         </Components.Paragraph>
-                             <Components.GhostButton onClick={() => toggle(false)}>
-                                 Sign Up
-                             </Components.GhostButton> 
-                       </Components.RightOverlayPanel>
-   
-                   </Components.Overlay>
-               </Components.OverlayContainer>
- 
-           </Components.Container>
- );
+    const [signIn, toggle] = React.useState(true);
+    return(
+        <Components.Container>
+            <Components.SignUpContainer signinIn={signIn}>
+                <Components.Form>
+                    <Components.Title>REGISTER</Components.Title>
+                    <Components.Input type='text' placeholder='Name' id='name'/>
+                    <Components.Input type='email' placeholder='Email' id='email'/>
+                    <Components.Input type='password' placeholder='Password' id='pass'/>
+                    <Components.Button>Sign Up</Components.Button>
+                </Components.Form>
+            </Components.SignUpContainer>
+
+            <Components.SignInContainer signinIn={signIn}>
+                <Components.Form>
+                    <Components.Title>LOGIN</Components.Title>
+                    <Components.Input type='email' placeholder='Email' id='email'/>
+                    <Components.Input type='password' placeholder='Password' id='pass'/>
+                    <Components.Button>Sign In</Components.Button>
+                    <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                </Components.Form>
+            </Components.SignInContainer>
+
+            <Components.OverlayContainer signinIn={signIn}>
+                <Components.Overlay signinIn={signIn}>
+
+                <Components.LeftOverlayPanel signinIn={signIn}>
+                    <Components.Title>Welcome Back!</Components.Title>
+                    <Components.Paragraph>
+                        To keep connected with us please login with your personal info!
+                    </Components.Paragraph>
+                    <Components.GhostButton onClick={() => toggle(true)}>
+                        Sign In
+                    </Components.GhostButton>
+                    </Components.LeftOverlayPanel>
+
+                    <Components.RightOverlayPanel signinIn={signIn}>
+                        <Components.Title>Hello, Friend!</Components.Title>
+                        <Components.Paragraph>
+                            Enter Your personal details and start journey with us!
+                        </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(false)}>
+                                Sign Up
+                            </Components.GhostButton> 
+                    </Components.RightOverlayPanel>
+
+                </Components.Overlay>
+            </Components.OverlayContainer>
+        </Components.Container>
+    );
 }
